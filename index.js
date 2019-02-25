df1a065374ba4d1059395529c7d4dc4bdb280c33
avidrutham <divyamathur89@gmail.com>
2019-02-25 13:23:08 +0530

    add authorization middleware to server

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express();
 const morgan = require("morgan");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const expressJwt = require("express-jwt");
 const PORT = process.env.PORT || 5000;
 
 app.use(morgan("dev"));
@@ -21,7 +22,20 @@ mongoose.connect(
   }
 );
 
-app.use("/todo", require("./routes/todo"));
+app.use((err, req, res, next) => {
+  console.log(err);
+  if (err.name === "UnauthorizedError") {
+    res.status(err.status);
+  }
+  return res.send({ message: err.message });
+});
+
+// Make the app use the express-jwt authentication middleware on anything starting with "/api"
+app.use("/api", expressJwt({ secret: process.env.SECRET }));
+
+app.use("/auth", require("./routes/auth"));
+
+app.use("/api/todo", require("./routes/todo"));
 
 app.use((err, req, res, next) => {
   console.error(err);
--- routes/auth.js
+++ routes/auth.js
@@ -6,14 +6,14 @@ const jwt = require("jsonwebtoken");
 // Post routes for User Signup & login
 
 authRouter.post("/signup", (req, res, next) => {
-  User.findOne({ username: req.body.username }, (err, existingUser) => {
+  User.findOne({ email: req.body.email }, (err, existingUser) => {
     if (err) {
       res.status(500);
       return next(err);
     }
     if (existingUser !== null) {
       res.status(400);
-      return next(new Error("That username already exists!"));
+      return next(new Error("That email is already registered!"));
     }
 
     const newUser = new User(req.body);
@@ -24,13 +24,15 @@ authRouter.post("/signup", (req, res, next) => {
       }
 
       const token = jwt.sign(user.toObject(), process.env.SECRET);
-      return res.status(201).send({ success: true, user: user.toObject() });
+      return res
+        .status(201)
+        .send({ success: true, user: user.toObject(), token });
     });
   });
 });
 
 authRouter.post("/login", (req, res, next) => {
-  User.findOne({ username: req.body.username.toLowerCase() }, (err, user) => {
+  User.findOne({ email: req.body.email }, (err, user) => {
     if (err) {
       return next(err);
     }
